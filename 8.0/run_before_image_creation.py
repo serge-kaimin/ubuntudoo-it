4834dc0117917c16404dc6b0f0b196af9cd8831f
Daphne Solutions <yes@daphne-solutions.com>
2017-05-05 10:53:36 +0200

    added gitignore

---
+++ 8.0/run_before_image_creation.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf l10n_it*
+
+rm -rf l10n-italy
+git clone -b 8.0 https://github.com/OCA/l10n-italy.git
+mv l10n-italy/l10n_it_* .
+rm -rf l10n-italy
+
+rm -rf l10n-italy-supplemental
+git clone -b 8.0 https://github.com/zeroincombenze/l10n-italy-supplemental.git
+mv l10n-italy-supplemental/l10n_it_* .
+rm -rf l10n-italy-supplemental
