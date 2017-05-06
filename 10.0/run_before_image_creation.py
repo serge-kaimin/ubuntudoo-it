2f3580ea97b60a87ac8848a289e259366b0ad88c
Daphne Solutions <yes@daphne-solutions.com>
2017-05-06 10:35:07 +0200

    working dockerimages for 8.0 and 10.0, including italian localization preprocessing

---
+++ 10.0/run_before_image_creation.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf addons
+rm -rf l10n_it*
+mkdir addons
+
+rm -rf l10n-italy
+git clone -b 10.0 https://github.com/OCA/l10n-italy.git
+mv l10n-italy/l10n_it_* ./addons
+rm -rf l10n-italy
+
+rm -rf l10n-italy-supplemental
+git clone -b 10.0 https://github.com/zeroincombenze/l10n-italy-supplemental.git
+mv l10n-italy-supplemental/l10n_it_* ./addons
+rm -rf l10n-italy-supplemental
