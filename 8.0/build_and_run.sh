c16a4c5a408944514129309350319b813c266721
Perevedko <aarychagov@gmail.com>
2017-11-08 01:23:42 +0400

    80 port for app

--- 8.0/build_and_run.sh
+++ 8.0/build_and_run.sh
@@ -7,7 +7,7 @@ IMAGE=ubuntudoo-it:8
 docker stop $CONTAINER
 docker rm $CONTAINER
 docker build -t $IMAGE .
-docker run -p 8069:8069 --name odoo8 --link db:db -t ubuntudoo-it:8
+docker run -v $(pwd):/etc/odoo -p 80:8069 --name odoo8 --link db:db -t ubuntudoo-it:8
 
 IP=$(echo $(hostname -I) | cut -d' ' -f 1)
 echo "App is running on:"
