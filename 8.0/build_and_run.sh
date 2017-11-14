e3bea047cee799f0ebe893c1bad41440055f282a
Alex <aarychagov@gmail.com>
2017-11-14 06:24:36 +0400

    volume for postgres and postgres

--- 8.0/build_and_run.sh
+++ 8.0/build_and_run.sh
@@ -2,6 +2,13 @@
 
 ./run_before_image_creation.sh
 
+# postgres
+docker create -v /var/lib/postgresql/data --name odoo_data busybox
+docker stop db
+docker rm db
+docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name db --volumes-from odoo_data postgres:9.4
+
+# app itself
 CONTAINER=odoo8
 IMAGE=ubuntudoo-it:8
 docker stop $CONTAINER
