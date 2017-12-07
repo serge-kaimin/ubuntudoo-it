9d6239da5c742bb92b4529644b2fb6b8224e5335
Daphne Solutions <yes@daphne-solutions.com>
2017-12-07 21:38:11 +0100

    added base_location modules

--- 8.0/run_before_image_creation.sh
+++ 8.0/run_before_image_creation.sh
@@ -21,14 +21,21 @@ rm -rf addons
 rm -rf l10n_it*
 mkdir addons
 
+rm -rf partner-contact
+git clone -b 8.0 https://github.com/OCA/partner-contact.git
+mv partner-contact/* ./addons
+rm -rf partner-contact
+
 rm -rf l10n-italy
 git clone -b 8.0 https://github.com/OCA/l10n-italy.git
-mv l10n-italy/l10n_it_* ./addons
+# mv l10n-italy/l10n_it_* ./addons
+mv l10n-italy/* ./addons
 rm -rf l10n-italy
 
 rm -rf l10n-italy-supplemental
 git clone -b 8.0 https://github.com/zeroincombenze/l10n-italy-supplemental.git
-mv l10n-italy-supplemental/l10n_it_* ./addons
+# mv l10n-italy-supplemental/l10n_it_* ./addons
+mv l10n-italy-supplemental/* ./addons
 rm -rf l10n-italy-supplemental
 
 python run_before_image_creation.py
