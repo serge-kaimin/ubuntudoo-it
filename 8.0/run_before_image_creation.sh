70fd8d7ff43ae502bc3a8460d2ae758d16726acb
Jothimani <jothimani1991@gmail.com>
2018-11-10 19:07:27 +0530

    Added  --depth 1 while clone github repos
    Reduce internet usage while cloning repositories every time by clone single depth of repo instead of all.

--- 8.0/run_before_image_creation.sh
+++ 8.0/run_before_image_creation.sh
@@ -22,40 +22,40 @@ rm -rf l10n_it*
 mkdir addons
 
 rm -rf partner-contact
-git clone -b 8.0 https://github.com/OCA/partner-contact.git
+git clone -b 8.0 https://github.com/OCA/partner-contact.git --depth 1
 mv partner-contact/* ./addons
 rm -rf partner-contact
 
 rm -rf l10n-italy
-git clone -b 8.0 https://github.com/OCA/l10n-italy.git
+git clone -b 8.0 https://github.com/OCA/l10n-italy.git --depth 1
 # mv l10n-italy/l* ./addons
 mv l10n-italy/l10n_it_* ./addons
 mv l10n-italy/account_invoice_entry_date ./addons
 rm -rf l10n-italy
 
 rm -rf l10n-italy-supplemental
-git clone -b 8.0 https://github.com/zeroincombenze/l10n-italy-supplemental.git
+git clone -b 8.0 https://github.com/zeroincombenze/l10n-italy-supplemental.git --depth 1
 # mv l10n-italy-supplemental/l10n_it_* ./addons
 mv l10n-italy-supplemental/* ./addons
 rm -rf l10n-italy-supplemental
 
 rm -rf stock-logistics-workflow
-git clone -b 8.0 https://github.com/OCA/stock-logistics-workflow.git
+git clone -b 8.0 https://github.com/OCA/stock-logistics-workflow.git --depth 1
 mv stock-logistics-workflow/stock* ./addons
 rm -rf stock-logistics-workflow
 
 rm -rf odoo_imppn
-git clone https://github.com/matteopolleschi/odoo_imppn.git
+git clone https://github.com/matteopolleschi/odoo_imppn.git --depth 1
 mv odoo_imppn ./addons
 rm -rf odoo_imppn
 
 rm -rf odoo_publishers
-git clone https://github.com/matteopolleschi/odoo_publishers.git
+git clone https://github.com/matteopolleschi/odoo_publishers.git --depth 1
 mv odoo_publishers ./addons
 rm -rf odoo_publishers
 
 rm -rf odoo_import_upwork
-git clone https://github.com/matteopolleschi/odoo_import_upwork.git
+git clone https://github.com/matteopolleschi/odoo_import_upwork.git --depth 1
 mv odoo_import_upwork ./addons
 rm -rf odoo_import_upwork
 
