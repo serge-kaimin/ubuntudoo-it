5126fef3cc9cba50c329658cb683f36c3af1ff3b
Sergey Kaimin <serge.kaimin@gmail.com>
2019-06-06 23:14:29 +0300

    Merge repositories to addons/ using rsync

--- dev/Build/run_before_image_creation.sh
+++ dev/Build/run_before_image_creation.sh
@@ -24,37 +24,51 @@ mkdir addons
 rm -rf l10n-italy
 git clone -b 12.0 https://github.com/OCA/l10n-italy.git
 # mv l10n-italy/l* ./addons
-mv l10n-italy/l10n_it_* ./addons
+#mv l10n-italy/l10n_it_* ./addons
+echo "Merge https://github.com/OCA/l10n-italy.git to addonns/"
+rsync -a l10n-italy/l10n_it_ ./addons
 rm -rf l10n-italy
 
 rm -rf partner-contact
 git clone -b 12.0 https://github.com/OCA/partner-contact.git
-mv partner-contact/* ./addons
+#mv partner-contact/* ./addons
+echo "Merge https://github.com/OCA/partner-contact.git to addonns/"
+rsync -a partner-contact/ ./addons
 rm -rf partner-contact
 
 rm -rf account-financial-tools
 git clone -b 12.0 https://github.com/OCA/account-financial-tools.git
-mv account-financial-tools/* ./addons
+#mv account-financial-tools/* ./addons
+echo "Merge https://github.com/OCA/account-financial-tools.git to addonns/"
+rsync -a account-financial-tools/ ./addons
 rm -rf account-financial-tools
 
 rm -rf account-financial-reporting
 git clone -b 12.0 https://github.com/OCA/account-financial-reporting.git
-mv account-financial-reporting/* ./addons
+#mv account-financial-reporting/* ./addons
+echo "Merge https://github.com/OCA/account-financial-reporting.git to addonns/"
+rsync -a account-financial-reporting/ ./addons
 rm -rf account-financial-reporting
 
 rm -rf server-ux
 git clone -b 12.0 https://github.com/OCA/server-ux.git
-mv server-ux/* ./addons
+#mv server-ux/* ./addons
+echo "Merge https://github.com/OCA/server-ux.git to addonns/"
+rsync -a server-ux/ ./addons
 rm -rf server-ux
 
 rm -rf crm
 git clone -b 12.0 https://github.com/OCA/crm.git
-mv crm/* ./addons
+#mv crm/* ./addons
+echo "Merge https://github.com/OCA/crm.git to addonns/"
+rsync -a crm/ ./addons
 rm -rf crm
 
 rm -rf odoo_publishers
 git clone -b 12.0 https://github.com/matteopolleschi/odoo_publishers.git
-mv odoo_publishers ./addons
+#mv odoo_publishers ./addons
+echo "Merge https://github.com/matteopolleschi/odoo_publishers.git to addonns/"
+rsync -a odoo_publishers/ ./addons
 rm -rf odoo_publishers
 
 #rm -rf l10n-italy-supplemental
